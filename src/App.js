15e92dd55aa8e573097e6b16349022e3f679a19d
N. M. Luna <nat.luna.64@gmail.com>
2023-03-17 09:33:05 -0700

    make skeleton app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <p>Welcome to BetterReads.</p>
+        <p>"It's like goodreads," she said. "But <i>better</i>".</p>
       </header>
+      <div className='nav-bar'>
+        <p><i className='fa-solid fa-house'></i> <i className='fa-solid fa-user'></i> <i class="fa-solid fa-magnifying-glass"></i></p>
+        <p>Welcome, fellow bookworm!</p>
+      </div>
+      <footer>
+        <p>(c) 2023 BetterReads</p>
+        <p><i class="fa-brands fa-facebook"></i> <i class="fa-brands fa-twitter"></i></p>
+      </footer>
     </div>
   );
 }
